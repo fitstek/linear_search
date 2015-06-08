5c9868763a47479832561b2420ba9901a65c5c4c
Fitsum Teklehaimanot <fitsum.teklehaimanot@gmail.com>
2015-06-08 23:56:58 +0100

    trying to make the generic code work for more data types

--- linearSearch/genericSearch.c
+++ linearSearch/genericSearch.c
@@ -9,14 +9,30 @@
 
 #include "genericSearch.h"
 
-void *genericSearch(void *key, void *base, int numOfElements, int elementSize ){
+//void *genericSearch(void *key, void *base, int numOfElements, int elementSize ){
+//  for (int i = 0; i < numOfElements; i++) {
+//    void *elementAddress = (char*) base + i * elementSize;
+//    // this method will work for bool, short, int, char, long, double and float because everuthing resides directly in the memory location
+//    // it will not work char*, strings, structs that have pointers inside
+//    if (memcmp(key, elementAddress, elementSize) == 0) {
+//      return elementAddress;
+//    }
+//  }
+//  return NULL;
+//}
+
+
+// The following implementation attempts to cover what the previous implementation has left out
+// Inorder to do that we have to replace memcmp with a custom function that we pass in as an argument
+
+void *genericSearch(void *key, void *base, int numOfElements, int elementSize, int (*intComparison(void *, void *))){
+  
   for (int i = 0; i < numOfElements; i++) {
-    void *elementAddress = (char*) base + i * elementSize;
-    // this method will work for bool, short, int, char, long, double and float because everuthing resides directly in the memory location
-    // it will not work char*, strings, structs that have pointers inside
-    if (memcmp(key, elementAddress, elementSize) == 0) {
+      void *elementAddress = base + i * elementSize;
+    if (intComparison(key, elementAddress) == 0) {
       return elementAddress;
     }
   }
   return NULL;
 }
+
--- linearSearch/main.c
+++ linearSearch/main.c
@@ -11,27 +11,42 @@
 
 int main(int argc, const char * argv[]) {
   void displayFoundElement(void *found);
+  int intComparison(void *element1, void *element2);
+//  int intComparisonFunction(key, element);
   
-  
-  int nonGenericLinearSearch(int key, int array[], int size);
-  void *genericSearch(void *key, void *base, int numOfElements, int elementSize );
+//  int nonGenericLinearSearch(int key, int array[], int size);
+  void *genericSearch(void *key, void *base, int numOfElements, int elementSize, int (*intComparison(void *, void *)));
+
   
   int myArray[5] = {1,2,3,4,5};
   int key = 6;
 
-  printf("The index of the searched integer: %i\n",nonGenericLinearSearch(key, myArray, sizeof(myArray)));
+//  printf("The index of the searched integer: %i\n",nonGenericLinearSearch(key, myArray, sizeof(myArray)));
   
   // To see the result from the first way of implementing the generic code
-  int *found = genericSearch(&key, &myArray, sizeof(myArray), sizeof(int));
+  int *found = genericSearch(&key, &myArray, sizeof(myArray), sizeof(int), intComparison(void*, void*));
 
   displayFoundElement(found);
 
 }
 
 void displayFoundElement(void *found){
-  if (found != NULL) {
+  if (found == 0) {
     printf("The character is found at memory location %p", found);
   } else {
     printf("No record found!!");
   }
+}
+
+//int intComparisonFunction(void* element1, void* element2){
+//  int *p1 = *element1;
+//  int *p2 = *element2;
+//  return -1;
+//}
+
+int intComparison(void *element1, void *element2){
+  int *ip1 = element1;
+  int *ip2 = element2;
+  
+  return *ip1 - *ip2;
 }
